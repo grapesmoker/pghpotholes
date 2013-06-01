f7dba5bc81e86d667d3f6e6b9256c23a6609161c
Jerry Vinokurov <jerryv@cmu.edu>
2013-06-01 14:21:55 -0400

    fixed typo

--- thinkathon/urls.py
+++ thinkathon/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from potholes.view import *
+from potholes.views import *
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -15,5 +15,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^thinkathon/', main)
+    url(r'^thinkathon/$', main),
+    url(r'^$', main),
 )

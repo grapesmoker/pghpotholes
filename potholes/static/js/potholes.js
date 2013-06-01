fbf7ad4772ff9fdcc2f15e94255dcc135c365624
Jerry Vinokurov <jerryv@cmu.edu>
2013-06-01 16:01:28 -0400

    leaflet

--- potholes/static/js/potholes.js
+++ potholes/static/js/potholes.js
@@ -2,8 +2,8 @@ $(function () {
 	
 	var map = L.map('map').setView([40.4406, -79.9961], 13);
 	
-	L.tileLayer('http://{s}.tile.cloudmade.com/556944c7a8de4e198895cbeae74a0eea/997/256/{z}/{x}/{y}.png', {
-    	key: 'API-key',
+	L.tileLayer('http://{s}.tile.cloudmade.com/{key}/997/256/{z}/{x}/{y}.png', {
+    	key: '556944c7a8de4e198895cbeae74a0eea',
     	styleId: 997
 	}).addTo(map);
 	

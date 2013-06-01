8a4516d0915107a63b970985f6ec8637a4a5601e
Jerry Vinokurov <jerryv@cmu.edu>
2013-06-01 16:37:25 -0400

    added pothole model

--- potholes/models.py
+++ potholes/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+import datetime as dt
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.utils import simplejson
+
+
+class Pothole(models.Model):
+    
+    lat = models.FloatField()
+    long = models.FloatField()
+    tweet_id = models.IntegerField()
+    reporter_id = models.IntegerField()
+    image = models.ImageField()
+    image_md5 = models.CharField(maxlength=32)
+    see_click_fix_url = models.URLField(required=False)
+    
+    

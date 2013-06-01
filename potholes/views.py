44357c4e6761dc1445b21a9187400451ddf0c414
Jerry Vinokurov <jerryv@cmu.edu>
2013-06-01 14:08:36 -0400

    added a basic view

--- potholes/views.py
+++ potholes/views.py
@@ -1,1 +1,22 @@
 # Create your views here.
+
+from models import *
+from forms import *
+
+from django.template.loader import get_template
+from django.template import Context, RequestContext
+from django.shortcuts import render_to_response
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.serializers import serialize
+from django.views.decorators.csrf import csrf_exempt
+from django.utils import simplejson
+
+def main(request):
+    
+    return render_to_response('main.html', 
+                              {'title': 'pghpotholes'},
+                              context_instance=RequestContext(request))
+    
+    

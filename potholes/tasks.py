248e90815eb06389862b05696e48e168bff8ddf5
Jerry Vinokurov <jerryv@cmu.edu>
2013-06-02 12:08:21 -0400

    hurrrr gotta add files hurrr

---
+++ potholes/tasks.py
@@ -0,0 +1,52 @@
+import tweepy
+from djcelery import celery
+from models import *
+
+class TwistStreamListener(tweepy.StreamListener):
+    
+    def __init__(self):
+        super(tweepy.StreamListener, self)
+        from thinkathon.auth_data import access_token_key, access_token_secret, consumer_key, consumer_secret
+        self.access_token_key = access_token_key
+        self.access_token_secret = access_token_secret
+        self.consumer_key = consumer_key
+        self.consumer_secret = consumer_secret
+        
+        self.auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+        self.auth.set_access_token(self.access_token_key, self.access_token_secret)
+    
+        self.hashtag = 'pghpotholes'
+        self.filter_timeout = 60
+        self.sleep_time1 = 6
+        self.sleep_time2 = 60
+    
+    def on_status(self, status):
+        try:
+            print "%s\t%s\t%s\t%s" % (status.text, 
+                                      status.author.screen_name, 
+                                      status.created_at, 
+                                      status.source,)
+        except Exception, e:
+            print 'Exception:', e
+            pass
+
+
+    def on_error(self, status_code):
+        print 'Error, status code:', status_code
+        return True # Don't kill the stream
+
+    def on_timeout(self):
+        print 'Timeout.'
+        return True # Don't kill the stream
+
+
+@celery.task()
+def twist_listener():
+    twist = TwistStreamListener()
+    streaming = tweepy.streaming.Stream(twist.auth, TwistStreamListener(), timeout = twist.filter_timeout)
+    print "Starting stream listener to look for hashtag "+hashtag
+    queryTerms = [hashtag]
+    streaming.filter(follow=None, track=queryTerms)
+    
+    
+    

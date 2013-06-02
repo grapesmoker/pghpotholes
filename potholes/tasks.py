e044f88af96515a61a20ce9dc4ad4da94e628685
Jerry Vinokurov <jerryv@cmu.edu>
2013-06-02 13:29:35 -0400

    twittah

--- potholes/tasks.py
+++ potholes/tasks.py
@@ -1,6 +1,10 @@
 import tweepy
 #from djcelery import celery
-#from models import *
+from models import Pothole
+
+from thinkathon.auth_data import access_token_key, access_token_secret, consumer_key, consumer_secret
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 
 class TwistStreamListener(tweepy.StreamListener):
     
@@ -9,10 +13,30 @@ class TwistStreamListener(tweepy.StreamListener):
     
     def on_status(self, status):
         try:
-            print "%s\t%s\t%s\t%s" % (status.text, 
-                                      status.author.screen_name, 
-                                      status.created_at, 
-                                      status.source,)
+            #print "%s\t%s\t%s\t%s" % (status.text, 
+            #                          status.author.screen_name, 
+            #                          status.created_at, 
+            #                          status.source,)
+            
+            geo = status.geo
+            if geo is None:
+                for p, v in vars(status).iteritems():
+                    print p, v
+                
+                tweet_id = status.id
+                username = status.from_user_name
+                api = tweepy.API(auth)
+                api.update_status('@{0} please turn on geolocation for twitter!'.format(username), tweet_id)
+                
+            else:
+                pothole = Pothole()
+                pothole.reporter_id = status.from_user_id
+                pothole.tweet_id = status.id
+                geo = status.geo
+                pothole.lat = geo['coordinates'][0]
+                pothole.long = geo['coordinates'][1]
+                pothole.save()
+            
         except Exception, e:
             print 'Exception:', e
             pass
@@ -30,10 +54,7 @@ class TwistStreamListener(tweepy.StreamListener):
 
 def twist_listener():
     twist = TwistStreamListener()
-    
-    from thinkathon.auth_data import access_token_key, access_token_secret, consumer_key, consumer_secret
         
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token_key, access_token_secret)
     
     hashtag = 'pghpotholes'
